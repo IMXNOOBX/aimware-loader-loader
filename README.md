a46c268fe44c7f4d1dbd8e7a108eea0a35932659
IMXNOOBX <paraelmobil403@gmail.com>
2022-12-02 19:49:39 +0100

    v1.2.5

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# aimware-loader-loader
-Aimware loader loader :D
+<div align="center"><img style="height: 100px;" src="assets/logo.png"></div>
+
+
+# 🎡 Aimware Loader Loader
+
+<div align="center">
+
+[![Build Status](https://cdn.discordapp.com/attachments/760822494419484672/1048308584578027560/image.png) <img src="https://img.shields.io/badge/Version-1.2.5-green.svg?style=flat&logo=github">
+
+</div>
+
+Simple electron app to load the aimware loader cause im to lazy to download it every time i need to run it :D 
+
+## 🪁 Simple Use
+
+***Developer Run***
+
+> npm run start
+
+***Build It Yourself***
+
+> npm run build
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
+const notifier = require('node-notifier');
 var { exec } = require("child_process");
 const ws = require("windows-shortcuts");
-// const { setTimeout } = require("timers/promises");
-const path = require("path");
 var fse = require('fs-extra');
+const path = require("path");
 const fs = require("fs");
-const { Console } = require("console");
 
 const root = process.env.APPDATA + "/aimware-loader";
 const lnk = process.env.APPDATA + "/Microsoft/Windows/Start Menu/Programs/Aimware.lnk";
-// const isDev = process.env.NODE_ENV !== "production";
-const developer = false;
+const developer = false; // set it manually
 var should_setup = false;
 var win;
 
@@ -37,66 +35,64 @@ const createWindow = () => {
 	else win.loadFile("render/index.html");
 };
 
+function windowsNotify(title, message) {
+	notifier.notify({ 
+		title, 
+		message, 
+		icon: path.join(__dirname, 'assets/logo.png'), 
+		sound: false, 
+		timeout: 3,
+		appID: 'Aimware-loader-loader',
+	});
+}
+
 ipcMain.on("setup:start", (e, options) => {
-	// console.log(options)
 
 	if (!fs.existsSync(options.path))
-		return console.log("Error: File dosent exist any more!");
+		return windowsNotify('Error', 'File dosent exist any more!')
 
 	fs.mkdirSync(root, { recursive: true });
 
 	exec(`xcopy /e /k /h /i . "${root}"`, function (err, data) {
 		if (err)
-			return console.log("Error: Could not copy the loader: " + err.message);
+			return windowsNotify('Error', `Could not copy the loader: ${err.message}`) 
 	});
 
 	fs.rename(options.path, root + "/aimware", function (err) {
 		if (err)
-			return console.log(
-				"Error: Could not make a copy of the loader. Check if your AV."
-			);
-		// console.log('Copied ' + options.path + ' to ' + root + '/aimware')
+			return windowsNotify('Error', `Could not make a copy of the loader. Check if your AV.`) 
 	});
 
 	ws.create(lnk, root + '/Aimware-loader-loader.exe');
 
-	// win.loadFile('render/index.html')
-	// win.webContents.send("setup:done");
+	windowsNotify('Success!', `Set up done! Enjoy the loader!`)
 	win.loadFile("render/index.html");
-	// console.log('Success: Setup finished!');
 });
 
 ipcMain.on("load:start", (e) => {
 	win.webContents.send("load:running");
-	// console.log("load:running");
 
 	if (!fs.existsSync(root))
-		return console.log(
-			"Error: Could not find the main folder. Open the program again."
-		);
+		return windowsNotify('Error', `Could not find the main folder. Open the program again.`) 
 
 	if (!fs.existsSync(root + "/aimware"))
-		return console.log(
-			"Error: Could not find aimware loader. Please reinstall the program."
-		);
+		return windowsNotify('Error', `Could not find aimware loader. Please reinstall the program.`) 
 
 	if (fs.existsSync(root + "/temp.exe")) fs.rmSync(root + "/temp.exe");
 
 	fs.copyFile(root + "/aimware", root + "/temp.exe", (err) => {
 		if (err)
-			return console.log(
-				"Error: Could not make a copy of the loader. Check if your AV and reinstall the program."
-			);
+			return windowsNotify('Error', `Could not make a copy of the loader. Check if your AV and reinstall the program.`) 
 	});
 
 	setTimeout(function () {
 		win.webContents.send("load:complete");
-		// if (fs.existsSync(root + "/temp.exe"))
-		// console.log(`${root}/temp.exe`)
-		exec(`start ${root}/temp.exe`, function (err, data) {
-			if (err)
-				return console.log("Error: Could not run the loader: " + err.message);
-		});
+		if (fs.existsSync(root + "/temp.exe"))
+			exec(`start ${root}/temp.exe`, function (err, data) {
+				if (err)
+					return windowsNotify('Error', `Could not run the loader: ${err.message}`) 
+			});
+			windowsNotify('Success!', `Loading Succesfull!`)
 		setTimeout(function () {
 			// app.quit();
 			win.hide()
@@ -106,7 +102,7 @@ ipcMain.on("load:start", (e) => {
 });
 
 app.whenReady().then(() => {
-	console.log(process.argv)
+	// console.log(process.argv)
 	if (process.argv.length > 1) {
 		for (i = 1; i < process.argv.length; i++) {
 			let argv = process.argv[i].toLowerCase();
@@ -114,7 +110,7 @@ app.whenReady().then(() => {
 				if (fs.existsSync(root)) fs.rmSync(root, { recursive: true });
 				if (fs.existsSync(lnk)) fs.rmSync(lnk);
 
-				console.log("App successfully removed!");
+				windowsNotify('Success!', `App successfully removed!`) 
 				return app.quit();
 			}
 		}
